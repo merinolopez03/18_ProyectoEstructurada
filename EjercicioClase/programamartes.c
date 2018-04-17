59b2d1b090904aea770271285ff0696bc98ba3fb
merino <pedromerino97@outlook.com>
2018-04-17 16:18:05 -0500

    este es extra

---
+++ EjercicioClase/programamartes.c
@@ -0,0 +1,24 @@
+/*-----------------------------------------------------------------------------*/
+/*autor:Pedro Merino Lopez                                                     */
+/*e-mail:pedromerino97@outlook.com                                             */
+/*fecha  de inicio: 17/04/2018                                 -               */
+/*fecha de actualizacion: 17/04/2018                                           */
+/*descripcion: programa que ordene un arreglo de numeros enteros de forma ascendente*/
+/*----------------------------------------------------------------------------------*/
+#include <stdio.h>
+#define TAM 5
+int int main()
+{
+const int array[TAM]
+int array1[TAM] = {1,2,3,4,5};
+int array2[TAM] = {1,2,[0]=10,3,4,5};
+int array3[] = {1,2,3,4,6};
+
+
+/*arreglo de caracteres */
+char char1[] = {"a","b","c"};
+char char2[] = {62,63,64};
+char char3[] = {0x63,0x64,0x65,0x66};
+printf("%c\n",char3[]);
+  return 0;
+}
---
+++ Extra/descendentes.c
@@ -0,0 +1,71 @@
+/*------------------------------------------------------*/
+/*autor:P edro Merino Lopez                           */
+/*e-mail:pedromerino97@outlook.com                     */
+/*fecha  de inicio: 14/04/2018                                     */
+/*fecha de actualizacion: 14/04/2018                                     */
+/*descripcion: programa que ordene un arreglo de numeros enteros de forma descentente*/
+/*------------------------------------------------------*/
+
+
+
+
+
+#include<stdio.h>                    /*declaracion de bibliotecas*/
+
+#define ARREGLO 100
+
+
+
+int main() {                  /*declaracion de la funcion principal*/
+
+
+	int aux;        /*se declaran las variables*/
+
+	int dim;
+
+	int i;
+
+	int j;
+
+	int num[ARREGLO];  /*se declara la dimension de num*/
+
+	dim = 10;              /*se declara el limite de dim */
+
+	for (i=0;i<=dim-1;i+=1) { /*se declara el inicio y limite del ciclo */
+
+		printf("dame el numero\n"); /*se imprime una peticion*/
+
+		scanf("%i",&num[i]);        /*se lee el dato*/
+
+
+
+	}
+
+	for (j=1;j<=dim-1;j+=1) {     /*se declara el inicio y limite del ciclo */
+
+		for (i=0;i<=dim-2;i+=1) {     /*se declara el inicio y limite del ciclo */
+
+			if (num[i]<num[i+1]) {       /*se pone una condicion */
+
+				aux = num[i+1];          /*el auxiliar toma el valor de num mas uno que es una posicion mas adelante */
+
+				num[i+1] = num[i];        /*una vez quedado en blanco toma el valor del que estaba antes de el */
+
+			num[i] = aux;   /*aca el valor inicial de num toma el valor del auxiliar */
+
+
+			}
+		}
+
+	}
+
+
+
+	for (i=0;i<=dim-1;i+=1) {   /*se declara el inicio y limite del ciclo */
+
+		printf("%i\n",num[i]);   /*imprime el orden */
+	}
+
+	return 0;                      /*copila con exito*/
+
+}
